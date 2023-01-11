ea2dd41ba8bd9f3c6e932ede9d70f128f43c4b64
ArifurLifeNColor <arifur@lifencolor.com>
2023-01-11 12:17:36 +0600

    Home page navbar and banner

---
+++ src/app/modules/landing/home/home.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from 'app/shared/shared.module';
+import { LandingHomeComponent } from 'app/modules/landing/home/home.component';
+import { landingHomeRoutes } from 'app/modules/landing/home/home.routing';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+    declarations: [
+        LandingHomeComponent,
+        NavbarComponent,
+        FooterComponent
+    ],
+    imports     : [
+        RouterModule.forChild(landingHomeRoutes),
+        MatButtonModule,
+        MatIconModule,
+        SharedModule
+    ]
+})
+export class LandingHomeModule
+{
+}
